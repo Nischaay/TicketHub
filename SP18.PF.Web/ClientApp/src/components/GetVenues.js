74cd2092fe0267a32f01d0619be642f990db73c8
Nischay Bist <nischay.bist@selu.edu>
2018-03-19 02:58:59 -0500

    added global searchbar and Venue list page with search with ID function

---
+++ SP18.PF.Web/ClientApp/src/components/GetVenues.js
@@ -0,0 +1,82 @@
+﻿import React, { Component } from 'react';
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import { SearchBar } from './SearchBar';
+export class GetVenues extends Component {
+    displayName = GetVenues.name
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            venues: [],
+            allVenues: [],
+            searchVenue: 0,
+            loading: true
+        };
+        this.fetchAll();
+
+    }
+
+    fetchAll() {
+        fetch('api/venues')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ venues: data, loading: false });
+                this.setState({ allVenues: data, loading: false });
+            });
+        this.searchVenues = this.searchVenues.bind(this);
+    }
+
+    searchVenues(id) {
+        if (!id) {
+            this.setState({ venues: this.state.allVenues, loading: false });
+        }
+        else {
+            fetch('api/venues/' + id)
+                .then(response => response.json())
+                .then(data => {
+                    console.log('search venue', data);
+                    const searchedVenues = this.state.allVenues.filter(x => x.id == data.id);
+                    this.setState({ venues: searchedVenues, loading: false });
+                });
+        }
+    }
+
+    static renderVenuesTable(venues) {
+        return (
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th> Id </th>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Capacity</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    {venues.map(venue =>
+                        <tr key={venue.id}>
+                            <td>{venue.id}</td>
+                            <td>{venue.name}</td>
+                            <td>{venue.description}</td>
+                            <td>{venue.capacity}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        );
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : GetVenues.renderVenuesTable(this.state.venues);
+        return (
+            <div>
+                <SearchBar onSearch={this.searchVenues} type="number" />
+                <h1>Venues</h1>
+                {contents}
+            </div>
+        );
+    }
+}
