dbc0f9994b375cc5e1790354abce084278a8534a
Nischay Bist <nischay.bist@selu.edu>
2018-03-27 02:30:59 -0500

    removed unnecessary changes

--- SP18.PF.Web/ClientApp/src/App.js
+++ SP18.PF.Web/ClientApp/src/App.js
@@ -31,18 +31,3 @@ export default class App extends Component {
         );
     }
 }
-
-//const PrivateRoute = ({ component: Component, ...rest }) => {
-//    return (
-//        <Route
-//            {...rest}
-//            render={props =>
-//                Authentication.isAuthenticated === true ? (
-//                    <Component {...props} />
-//                ) : (
-//                        <Redirect
-//                            to={{ pathname: "/login", state: { from: props.location } }}
-//                        />
-//                    )}
-//        />);
-//};
